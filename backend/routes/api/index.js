7a0346c3943e80902c38512dba2357e9f5f01bb2
Steph Meyering <stephane.meyering@gmail.com>
2025-01-17 23:24:55 -0800

    add token cookie testing route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,22 @@
-// backend/routes/api/index.js
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });  
+
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Barack Obama'
+      }
+    });
+  setTokenCookie(res, user);
+  console.log(setTokenCookie(res, user))
+  return res.json({ user });
+}));
 
 module.exports = router;
