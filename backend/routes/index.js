a376fe216bbc265ae8b40a2593d3131c4769c319
Steph Meyering <stephane.meyering@gmail.com>
2025-02-16 21:37:04 -0800

    remove hello world route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,12 +4,4 @@ const router = express.Router();
 const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function(req, res) {
-  // Call req.csrfToken() to generate the token
-  const csrfToken = req.csrfToken();
-  res.cookie('XSRF-TOKEN', csrfToken);
-  console.log("CSRF TOKEN:", csrfToken);
-  res.send('Hello World!');
-});
-
 module.exports = router;
