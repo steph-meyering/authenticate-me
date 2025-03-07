3c380de5aa78c43885582578673062691b588362
Steph Meyering <stephane.meyering@gmail.com>
2025-03-07 11:47:26 -0800

    add login component and routing logic in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage"; 
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+      <Routes>
+        <Route path="/login" element={<LoginFormPage />}/>
+      </Routes>
   );
 }
 
