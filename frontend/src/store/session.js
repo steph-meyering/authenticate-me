750d006845006cc27e6d31c20025a6ad2c89183a
Steph Meyering <stephane.meyering@gmail.com>
2025-03-07 14:45:01 -0800

    update session reducer to account for CREATE_USER action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -62,6 +62,10 @@ const sessionReducer = (state = initialState, action) => {
       newState = Object.assign({}, state);
       newState.user = action.payload;
       return newState;
+    case CREATE_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.payload;
+      return newState;
     case REMOVE_USER:
       newState = Object.assign({}, state);
       newState.user = null;
